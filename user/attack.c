2800e17a1694456dfe6a29987a71df5d0f8bdf16
pesi.taototo <ptaototo123@gmail.com>
2025-10-24 16:20:38 -0700

    completed attack xv6

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,23 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define DATASIZE (8*4096)
+
 int
 main(int argc, char *argv[])
 {
   // Your code here.
+  sbrk(16);
+  char *att = sbrk(DATASIZE);
+
+  int help = strcmp("This may help.", att);
+  if (help == 0) {
+    // printf("HELP!\n");
+    printf("%s\n", (att+16));
+    
+  }
+
+  // printf("%s\n", (att));
 
   exit(1);
 }
